3948ffcbd0ddf1fbb3c72e8d1ed8fbc30583fb2e
fomalhautsanz <johartrixsalada@gmail.com>
2026-09-21 09:12:14 +0800

    fixed staff login page

--- my-project/resources/views/staff/login.blade.php
+++ my-project/resources/views/staff/login.blade.php
@@ -1,9 +1,1 @@
-@extends('layouts.staff')
-
-@section('content')
-    @include('staff.pages._login')
-@endsection
-
-@section('scripts')
-    @include('staff.scripts._staff_scripts')
-@endsection
+@include('staff.pages._login')
